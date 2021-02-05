87ede817f48dc4a240f6886286fa4194afce0240
yichunzhao <zhaoyichun@hotmail.com>
2021-02-05 08:01:46 +0100

    deactivate a test

--- src/test/java/com/ynz/fin/average233day/service/AllNasStocksHandlerTest.java
+++ src/test/java/com/ynz/fin/average233day/service/AllNasStocksHandlerTest.java
@@ -1,5 +1,6 @@
 package com.ynz.fin.average233day.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ class AllNasStocksHandlerTest {
     private AllNasStocksHandler stocksHandler;
 
     @Test
+    @Disabled
     void testRunFirst10Stocks() {
         stocksHandler.calAllNasStock20Days233DaysIncremental();
     }
